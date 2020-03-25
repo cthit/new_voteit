36b4844b4b1ecdad01e0eac37ef88a0cefb76b65
Swexbe <erik@erikjohnsson.se>
2020-03-25 14:38:45 +0100

    SMTP settings as environment variables

--- prod.sh
+++ prod.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+#Fix env variables
+sed -i "s/%SMTP_HOST%/${SMTP_HOST}"
+sed -i "s/%SMTP_PORT%/${SMTP_PORT}"
+sed -i "s/%SMTP_SENDER%/${SMTP_SENDER}"
+
 ./bin/supervisord -c etc/supervisord.conf
 nginx
 while :; do read; done < /dev/null
