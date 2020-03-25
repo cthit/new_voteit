d792999fc5f127e30a86a6630acc16509c79c4cd
Swexbe <erik@erikjohnsson.se>
2020-03-25 14:40:10 +0100

    Fix prod.sh

--- prod.sh
+++ prod.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #Fix env variables
-sed -i "s/%SMTP_HOST%/${SMTP_HOST}"
-sed -i "s/%SMTP_PORT%/${SMTP_PORT}"
-sed -i "s/%SMTP_SENDER%/${SMTP_SENDER}"
+sed -i "s/%SMTP_HOST%/${SMTP_HOST}/g" ./etc/production.ini
+sed -i "s/%SMTP_PORT%/${SMTP_PORT}/g" ./etc/production.ini
+sed -i "s/%SMTP_SENDER%/${SMTP_SENDER}/g" ./etc/production.ini
 
 ./bin/supervisord -c etc/supervisord.conf
 nginx
