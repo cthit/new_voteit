7038f67bf358aef97452fe62ffe541e4d41a14ef
Swexbe <erik@erikjohnsson.se>
2020-03-25 14:59:10 +0100

    Supervisord output as docker output

--- prod.sh
+++ prod.sh
@@ -5,6 +5,5 @@ sed -i "s/%SMTP_HOST%/${SMTP_HOST}/g" ./etc/production.ini
 sed -i "s/%SMTP_PORT%/${SMTP_PORT}/g" ./etc/production.ini
 sed -i "s/%SMTP_SENDER%/${SMTP_SENDER}/g" ./etc/production.ini
 
-./bin/supervisord -c etc/supervisord.conf
 nginx
-while :; do read; done < /dev/null
+./bin/supervisord -n -c etc/supervisord.conf
